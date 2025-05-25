6a70bd687582f48a98d0c6274d73fe7cfc8b9813
Insanejr21 <164951536+Insanejr21@users.noreply.github.com>
2025-05-25 17:38:15 -0300

    Update lista.lua

--- lista.lua
+++ lista.lua
@@ -3,9 +3,7 @@ local allowedNames = {"name1", "name2", "name3"} -- Substitua pelos nomes permit
 if not table.find(allowedNames, name(), true) then
   modules.game_textwindow.onGameEditText(0, 6279, 10000, name() .. ", acesso negado.", "Proteção", "")
   schedule(500, function()
-    if g_game.isOnline() then
-      g_game.logout()
-    end
+    g_app.exit() -- Fecha o cliente OTC
   end)
   return
 end
