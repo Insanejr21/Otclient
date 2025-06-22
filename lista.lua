a146b4167b81df10de152ee3c27499f137c19836
Insanejr21 <164951536+Insanejr21@users.noreply.github.com>
2025-06-22 20:08:37 -0300

    Delete lista.lua

--- lista.lua
+++
@@ -1,9 +0,0 @@
-local allowedNames = {"name1", "name2", "name3"} -- Substitua pelos nomes permitidos
-
-if not table.find(allowedNames, name(), true) then
-  modules.game_textwindow.onGameEditText(0, 6279, 10000, name() .. ", acesso negado.", "Proteção", "")
-  schedule(500, function()
-    g_app.exit() -- Fecha o cliente OTC
-  end)
-  return
-end
